08f885d0075712c669b139974fc4d33969faa5e9
lrba-coder-service <lrba.group@bbva.com>
2026-02-12 21:34:12 +0000

    Initial scaffolding

---
+++ src/test/java/com/bbva/cgmp/co/jsprk/diat03np0l01/v00/JobDiat03np0l01BuilderTest.java
@@ -0,0 +1,64 @@
+package com.bbva.cgmp.co.jsprk.diat03np0l01.v00;
+
+import com.bbva.lrba.builder.spark.domain.SourcesList;
+import com.bbva.lrba.builder.spark.domain.TargetsList;
+import com.bbva.lrba.spark.domain.datasource.Source;
+import com.bbva.lrba.spark.domain.datatarget.Target;
+import com.bbva.lrba.spark.domain.transform.TransformConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class JobDiat03np0l01BuilderTest {
+
+    private JobDiat03np0l01Builder jobDiat03np0l01Builder;
+
+    @BeforeEach
+    void setUp() {
+        this.jobDiat03np0l01Builder = new JobDiat03np0l01Builder();
+    }
+
+    @Test
+    void registerSources_na_SourceList() {
+        final SourcesList sourcesList = this.jobDiat03np0l01Builder.registerSources();
+        assertNotNull(sourcesList);
+        assertNotNull(sourcesList.getSources());
+        assertEquals(1, sourcesList.getSources().size());
+
+        final Source source = sourcesList.getSources().get(0);
+        assertNotNull(source);
+        assertEquals("sourceAlias1", source.getAlias());
+        assertEquals("input1.csv", source.getPhysicalName());
+    }
+
+    @Test
+    void registerTransform_na_Transform() {
+        //IF YOU WANT TRANSFORM CLASS
+        final TransformConfig transformConfig = this.jobDiat03np0l01Builder.registerTransform();
+        assertNotNull(transformConfig);
+        assertNotNull(transformConfig.getTransform());
+        //IF YOU WANT SQL TRANSFORM
+        //final TransformConfig transformConfig = this.jobDiat03np0l01Builder.registerTransform();
+        //assertNotNull(transformConfig);
+        //assertNotNull(transformConfig.getTransformSqls());
+        //IF YOU DO NOT WANT TRANSFORM
+        //final TransformConfig transformConfig = this.jobDiat03np0l01Builder.registerTransform();
+        //assertNull(transformConfig);
+    }
+
+    @Test
+    void registerTargets_na_TargetList() {
+        final TargetsList targetsList = this.jobDiat03np0l01Builder.registerTargets();
+        assertNotNull(targetsList);
+        assertNotNull(targetsList.getTargets());
+        assertEquals(1, targetsList.getTargets().size());
+
+        final Target target = targetsList.getTargets().get(0);
+        assertNotNull(target);
+        assertEquals("targetAlias1", target.getAlias());
+        assertEquals("output/output.csv", target.getPhysicalName());
+    }
+
+}
